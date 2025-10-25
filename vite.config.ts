ca1652f236cc39307e2271d9e34f0c76360023c7
sadiqJ20 <sadiqj8227@gmail.com>
2025-10-25 10:57:46 +0530

    fix: update Vercel and Vite config for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? './' : '/',
+  base: '/',
   build: {
     outDir: "./dist",
     emptyOutDir: true,
